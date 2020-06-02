c4fc15c970f337fb23d64f7d25b6c04baaeaf6d4
katguerin <katguerin55@gmail.com>
2020-06-01 20:38:50 -0700

    wasn't sure if i was doing this correctly so i started over from mid 12.2.4

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-
 const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -14,33 +12,13 @@ const db = new sqlite3.Database('./db/theshop.db', err => {
     if (err) {
       return console.error(err.message);
     }
-  
     console.log('Officially connected to the shop database.');
 });
 
+// Print in terminal all from department table
 db.all(`SELECT * FROM department`, (err, rows) => {
     console.log(rows);
   });
-
-// Delete an employee
-// db.run(`DELETE FROM employee WHERE id = ?`, 1, function(err, result) {
-//     if (err) {
-//       console.log(err);
-//     }
-//     console.log(result, this, this.changes);
-// });
-
-// Create an employee
-// const sql = `INSERT INTO employee (id, first_name, last_name, industry_connected) 
-//               VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-// // ES5 function, not arrow function, to use this
-// db.run(sql, params, function(err, result) {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result, this.lastID);
-// });
 
 // Default response for any other requests(Not Found) Catch all
 app.use((req, res) => {
